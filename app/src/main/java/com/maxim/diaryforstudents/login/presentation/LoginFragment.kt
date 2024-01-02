568ad066a6b4f53204ac624ee2698fff0976bbb5
Diryfort <dorhungmi2@gmail.com>
2024-01-02 15:03:42 +0300

    little fix

--- app/src/main/java/com/maxim/diaryforstudents/login/presentation/LoginFragment.kt
+++ app/src/main/java/com/maxim/diaryforstudents/login/presentation/LoginFragment.kt
@@ -6,13 +6,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.activity.result.contract.ActivityResultContracts
 import com.maxim.diaryforstudents.core.presentation.BaseFragment
-import com.maxim.diaryforstudents.core.sl.ProvideViewModel
 import com.maxim.diaryforstudents.databinding.FragmentLoginBinding
 import com.maxim.diaryforstudents.login.data.AuthResultWrapper
 
 class LoginFragment : BaseFragment<FragmentLoginBinding, LoginViewModel>() {
-    override val viewModelClass: Class<LoginViewModel>
-        get() = LoginViewModel::class.java
+    override val viewModelClass = LoginViewModel::class.java
 
     override fun bind(inflater: LayoutInflater, container: ViewGroup?) =
         FragmentLoginBinding.inflate(inflater, container, false)
@@ -24,7 +22,6 @@ class LoginFragment : BaseFragment<FragmentLoginBinding, LoginViewModel>() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewModel = (activity as ProvideViewModel).viewModel(LoginViewModel::class.java)
 
         viewModel.observe(this) {
             it.show(binding.loginButton, binding.progressBar, binding.errorTextView)
