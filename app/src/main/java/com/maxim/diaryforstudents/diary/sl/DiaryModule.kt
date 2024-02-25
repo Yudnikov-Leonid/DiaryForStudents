2c05fbeb916be399ada5425943a1c3324f61cf67
Diryfort <dorhungmi2@gmail.com>
2024-02-25 22:21:44 +0300

    feature/new-diary remove filters from module

--- app/src/main/java/com/maxim/diaryforstudents/diary/sl/DiaryModule.kt
+++ app/src/main/java/com/maxim/diaryforstudents/diary/sl/DiaryModule.kt
@@ -6,49 +6,21 @@ import com.maxim.diaryforstudents.core.sl.Core
 import com.maxim.diaryforstudents.core.sl.Module
 import com.maxim.diaryforstudents.diary.data.DayDataToDomainMapper
 import com.maxim.diaryforstudents.diary.data.DiaryDataToDomainMapper
-import com.maxim.diaryforstudents.diary.domain.DiaryInteractor
 import com.maxim.diaryforstudents.diary.data.DiaryRepository
 import com.maxim.diaryforstudents.diary.data.DiaryService
 import com.maxim.diaryforstudents.diary.domain.DayDomainToUiMapper
 import com.maxim.diaryforstudents.diary.domain.DiaryDomainToUiMapper
+import com.maxim.diaryforstudents.diary.domain.DiaryInteractor
 import com.maxim.diaryforstudents.diary.presentation.DiaryCommunication
-import com.maxim.diaryforstudents.diary.presentation.DiaryUi
 import com.maxim.diaryforstudents.diary.presentation.DiaryViewModel
 import com.maxim.diaryforstudents.performance.common.data.FailureHandler
 import com.maxim.diaryforstudents.performance.common.data.PerformanceDataToDomainMapper
 import com.maxim.diaryforstudents.performance.common.domain.PerformanceDomainToUiMapper
-import com.maxim.diaryforstudents.performance.common.presentation.PerformanceUi
 
 class DiaryModule(private val core: Core, private val clear: ClearViewModel) :
     Module<DiaryViewModel> {
 
-    private val homeworkFilter = object : DiaryUi.Mapper<Boolean> {
-        override fun map(
-            name: String, topic: String, homework: String,
-            startTime: String, endTime: String, date: Int, marks: List<PerformanceUi.Mark>
-        ) = homework.isNotEmpty()
-    }
-    private val topicFilter = object : DiaryUi.Mapper<Boolean> {
-        override fun map(
-            name: String, topic: String, homework: String,
-            startTime: String, endTime: String, date: Int, marks: List<PerformanceUi.Mark>
-        ) = topic.isNotEmpty()
-    }
-    private val marksFilter = object : DiaryUi.Mapper<Boolean> {
-        override fun map(
-            name: String, topic: String, homework: String,
-            startTime: String, endTime: String, date: Int, marks: List<PerformanceUi.Mark>
-        ) = marks.isNotEmpty()
-    }
-    private val nameFilter = object : DiaryUi.Mapper<Boolean> {
-        override fun map(
-            name: String, topic: String, homework: String,
-            startTime: String, endTime: String, date: Int, marks: List<PerformanceUi.Mark>
-        ) = true
-    }
-
     override fun viewModel() = DiaryViewModel(
-        mutableListOf(homeworkFilter, topicFilter, marksFilter, nameFilter),
         DiaryInteractor.Base(
             DiaryRepository.Base(
                 core.retrofit().create(DiaryService::class.java),
